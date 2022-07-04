df701c36bab8a998f695633da0e2e1dbcc2bfdf1
Kevin Gilbert Toding <122202002832@mhs.dinus.ac.id>
2022-07-03 21:00:28 -0300

    update api and database

--- Database/data_barang.sql
+++ Database/data_barang.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 27, 2022 at 01:26 PM
+-- Generation Time: Jul 04, 2022 at 01:58 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.0.13
 
@@ -32,15 +32,26 @@ CREATE TABLE `data_barang` (
   `nama_barang` varchar(80) NOT NULL,
   `harga` int(10) NOT NULL,
   `jumlah` int(10) NOT NULL,
-  `satuan` varchar(50) NOT NULL
+  `satuan` varchar(50) NOT NULL,
+  `image` varchar(300) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `data_barang`
 --
 
-INSERT INTO `data_barang` (`kode`, `nama_barang`, `harga`, `jumlah`, `satuan`) VALUES
-('B089283', 'AirPods Pro', 3500000, 232, 'Unit');
+INSERT INTO `data_barang` (`kode`, `nama_barang`, `harga`, `jumlah`, `satuan`, `image`) VALUES
+('32h23sda', 'sepeda motor', 23000000, 2, 'unit', '32h23sda.png'),
+('b0909', 'speaker', 500000, 67, 'unit', 'b0909.png'),
+('B23921', 'AirPods Pro', 3500000, 74, 'Unit', 'B23921.png'),
+('B343990', 'Apple Watch S7 Black', 7000000, 85, 'Unit', 'B343990.png'),
+('b732932', 'charger', 320000, 87, 'unit', 'b732932.png'),
+('B7823', 'MacBook Pro 14', 19000000, 44, 'Unit', 'B7823.png'),
+('b8023911', 'iPhone 13 Pro Max', 18000000, 23, 'Unit', 'b8023911.png'),
+('B93321', 'AirPods Pro Max', 5200000, 58, 'Unit', 'B93321.png'),
+('BA82212', 'Apple Watch S7 Beige', 7000000, 45, 'Unit', 'BA82212.png'),
+('bh6327', 'Jacket', 2000000, 23, 'Unit', ''),
+('sadasd', 'air minum', 30000, 23, 'unit', 'sadasd.png');
 
 --
 -- Indexes for dumped tables
