b4686bc08fadac36f5e5a280c82618baef8660d6
KEVINGILBERTTODING <122202002832@mhs.dinus.ac.id>
2022-07-16 06:01:56 +0700

    added logout button

--- app/src/main/java/com/example/productscanner/AboutMe.java
+++ app/src/main/java/com/example/productscanner/AboutMe.java
@@ -4,9 +4,12 @@ import static com.bumptech.glide.request.RequestOptions.bitmapTransform;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
@@ -16,12 +19,14 @@ import jp.wasabeef.glide.transformations.BlurTransformation;
 public class AboutMe extends AppCompatActivity {
 
     ImageView imgDetail;
+    SharedPreferences sharedpreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_me);
         imgDetail   = findViewById(R.id.det_image);
+        Button btnLogout = findViewById(R.id.btn_logout);
 
         hideNavigationBar();
 
@@ -33,6 +38,17 @@ public class AboutMe extends AppCompatActivity {
                 // set image blur
                 .apply(bitmapTransform(new BlurTransformation(20)))
                 .into(imgDetail);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences preferences =getSharedPreferences("logindata", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.clear();
+                editor.apply();
+                finish();
+            }
+        });
 
     }
 
