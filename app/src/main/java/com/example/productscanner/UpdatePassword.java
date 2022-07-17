fb3325d6a04ea13d72a2a79ea83763252001e5ef
KEVINGILBERTTODING <122202002832@mhs.dinus.ac.id>
2022-07-17 14:52:59 +0700

    [SOLVED] update password validation

--- app/src/main/java/com/example/productscanner/UpdatePassword.java
+++ app/src/main/java/com/example/productscanner/UpdatePassword.java
@@ -48,16 +48,34 @@ public class UpdatePassword extends AppCompatActivity {
             public void onClick(View v) {
 
 
+                // Mengambil data dari form
                 String usernamex = et_username.getText().toString();
                 String password = et_password.getText().toString();
-                if (username.isEmpty() || password.isEmpty()) {
-                    Toast.makeText(UpdatePassword.this, "Username atau Password tidak boleh kosong", Toast.LENGTH_SHORT).show();
-                } else {
+                String password_old = et_password_old.getText().toString();
+
+                // Jika username dan password kosong
+                if (username.isEmpty() || password.isEmpty() || password_old.isEmpty()) {
+                    Toast.makeText(UpdatePassword.this, "Password tidak boleh kosong", Toast.LENGTH_SHORT).show();
+                }
+
+                // Jika jumlah chracter password kurang dari 8
+                if (password.length() < 8 || password_old.length() < 8) {
+                    et_password_old.requestFocus();
+                    Toast.makeText(UpdatePassword.this, "Password harus terdiri dari 8 character atau lebih", Toast.LENGTH_SHORT).show();
+                }
+                else {
                     refUsers.addListenerForSingleValueEvent(new ValueEventListener() {
                         @Override
                         public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                            // Cek jika username telah terdaftar di firebase database
+
                             if (snapshot.hasChild(usernamex)) {
+                                // Mmengambil data password dari firebase
                                 String password_old = snapshot.child(usernamex).child("password").getValue(String.class);
+
+                                // Jika password lama yang diinputkan sama dengan password yang ada di firebase
+
                                 if (password_old.equals(et_password_old.getText().toString())) {
                                     refUsers.child(usernamex).child("password").setValue(password);
                                     Toast.makeText(UpdatePassword.this, "Password berhasil diubah", Toast.LENGTH_SHORT).show();
