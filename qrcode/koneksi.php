834d6ffad439988bf84b7ad1bac3f87315481388
KEVINGILBERTTODING <122202002832@mhs.dinus.ac.id>
2022-07-21 00:08:55 +0700

    [ADD] server api and database

--- qrcode/koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('host', 'localhost');
-define('name', 'root');
-define('pass', '');
-define('dbase', 'qrcode');
-
-$conn = mysqli_connect(host, name, pass, dbase) or die('Unable to connect');
